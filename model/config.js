b20a00717faa4be64e23929eb3dc7ca6ec03c1cc
Thoragal <94929559+Thoragal@users.noreply.github.com>
2026-07-09 21:59:46 +0200

    Update SERVICE_URL in config.js

--- model/config.js
+++ model/config.js
@@ -2,6 +2,6 @@ sap.ui.define([], function () {
 	"use strict";
 
 	return {
-		SERVICE_URL: "https://nodejs-production-1b56.up.railway.app"
+		SERVICE_URL: "nodejs20prod-production.up.railway.app"
 	};
 });
